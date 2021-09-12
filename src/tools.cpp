c927c16453ba5a7f3aeae8f0f55920feb4af5dea
Mwesi <mwesi@yahoo.com>
2021-09-12 09:17:06 -0400

    Update to tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -13,39 +13,36 @@ Tools::~Tools() {}
 
 
 VectorXd CalculateRMSE(const vector<VectorXd> &estimations,
-    const vector<VectorXd> &ground_truth);
-
-int main() {
+    const vector<VectorXd> &ground_truth) {
   /**
    * Compute RMSE
    */
-  vector<VectorXd> estimations;
-  vector<VectorXd> ground_truth;
+
   VectorXd rmse(4);
+  rmse << 0,0,0,0;
+  VectorXd rmse_precal(4);
+  rmse_precal << 0,0,0,0;
 
   // the input list of estimations
-  VectorXd e(4);
-  e << 1, 1, 0.2, 0.1;
-  estimations.push_back(e);
-  e << 2, 2, 0.3, 0.2;
-  estimations.push_back(e);
-  e << 3, 3, 0.4, 0.3;
-  estimations.push_back(e);
 
-  // the corresponding list of ground truth values
-  VectorXd g(4);
-  g << 1.1, 1.1, 0.3, 0.2;
-  ground_truth.push_back(g);
-  g << 2.1, 2.1, 0.4, 0.3;
-  ground_truth.push_back(g);
-  g << 3.1, 3.1, 0.5, 0.4;
-  ground_truth.push_back(g);
+  for(unsigned int i=0; i < estimations.size(); ++i)
+  {
+  
+    VectorXd_dt = estimations[i] - ground_truth[i];
+      delta = delta.array()*delta.array();
+      rmse_precal += delta;
+    
+  }
 
+  rmse = rmse_pecal / n;
+  rmse = rmse.array().sqrt();
+  
+  return rmse;
   // call the CalculateRMSE and print out the result
-  cout << CalculateRMSE(estimations, ground_truth) << endl;
+  //cout << CalculateRMSE(estimations, ground_truth) << endl;
 
 
-  return 0;
+  //return 0;
 }
 
 VectorXd CalculateRMSE(const vector<VectorXd> &estimations,
@@ -84,9 +81,7 @@ VectorXd CalculateRMSE(const vector<VectorXd> &estimations,
 }
 
 
-MatrixXd CalculateJacobian(const VectorXd& x_state);
-
-int main() {
+MatrixXd CalculateJacobian(const VectorXd& x_state) {
   /**
    * Compute the Jacobian Matrix
    */
