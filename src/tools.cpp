805fdb0f8e60acce99e72e6753104432ee3695f6
Mwesi <mwesi@yahoo.com>
2021-09-12 16:09:08 -0400

    All Code now compiles :)

--- src/tools.cpp
+++ src/tools.cpp
@@ -12,7 +12,7 @@ Tools::Tools() {}
 Tools::~Tools() {}
 
 
-VectorXd CalculateRMSE(const vector<VectorXd> &estimations,
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
     const vector<VectorXd> &ground_truth) {
   /**
    * Compute RMSE
