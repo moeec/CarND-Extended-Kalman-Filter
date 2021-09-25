3aa6689bc0fdd681683f4e0ac0e6de59a4eebfe1
Mwesi <mwesi@yahoo.com>
2021-09-25 06:06:09 -0400

    Update kalman_filter.cpp

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -14,24 +14,40 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+  x_ = x_in;  // object state
+  P_ = P_in;  // object covariance matrix
+  F_ = F_in;  // state transition matrix
+  H_ = H_in;  // measurement matrix
+  R_ = R_in;  // measurement covariance matrix
+  Q_ = Q_in; // process covariance matrix
 }
 
 void KalmanFilter::Predict() {
   /**
-   * TODO: predict the state
+   * TODO: predict the state  
+   * u is external motion
    */
+  u = VectorXd(2);
+  u << 0, 0;
+  x = F * x + u;
+  MatrixXd Ft = F.transpose();
+  P = F * P * Ft + Q;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+  VectorXd y = z - H * x;
+  MatrixXd Ht = H.transpose();
+  MatrixXd S = H * P * Ht + R;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P * Ht * Si;
+  
+  // new state
+  x = x + (K * y);
+  P = (I - K * H) * P;
+  
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
