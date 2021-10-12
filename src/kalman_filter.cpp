794ea394453242f5af8260e91a15de5851c455f7
Mwesi <mwesi@yahoo.com>
2021-10-12 06:22:37 -0400

    Update kalman_filter.cpp

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -27,32 +27,67 @@ void KalmanFilter::Predict() {
    * TODO: predict the state  
    * u is external motion
    */
-  u = VectorXd(2);
-  u << 0, 0;
-  x = F * x + u;
-  MatrixXd Ft = F.transpose();
-  P = F * P * Ft + Q;
+  x_ = F_ * x_ ;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
-  VectorXd y = z - H * x;
-  MatrixXd Ht = H.transpose();
-  MatrixXd S = H * P * Ht + R;
-  MatrixXd Si = S.inverse();
-  MatrixXd K =  P * Ht * Si;
   
   // new state
-  x = x + (K * y);
-  P = (I - K * H) * P;
+  
+  VectorXd y = z - H_ * x_;
+ 
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_ * Ht * Si;
+  
+  // New state
+  x_ = x_ + (K * y);
+  int x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
   
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Extended Kalman Filter equations
-   *
-   */
+  
+  // TODO: update the state by using Extended Kalman Filter equations
+
+    // get components of predicted state
+    float px = x_(0);
+    float py = x_(1);
+    float vx = x_(2);
+    float vy = x_(3);
+
+    // get components of radar measurement space
+    float rho = sqrt(px * px + py * py);
+    float phi = atan2(py, px);
+    float rho_dot = (rho != 0 ? (px * vx + py * vy) / rho : 0);
+
+    // define predicted position and speed
+    VectorXd z_pred(3);
+    z_pred << rho, phi, rho_dot;
+
+    // measurement update
+    VectorXd y = z - z_pred;
+
+    // normalize angle
+    double width = 2 * M_PI;   //
+    double offsetValue = y(1) + M_PI;   // value relative to 0
+    y(1) = (offsetValue - (floor(offsetValue / width) * width)) - M_PI;
+
+    MatrixXd PHt = P_ * H_.transpose();
+    MatrixXd S = H_ * PHt + R_;
+    MatrixXd K = PHt * S.inverse();
+
+    // new state
+    x_ = x_ + (K * y);
+    int x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
 }
